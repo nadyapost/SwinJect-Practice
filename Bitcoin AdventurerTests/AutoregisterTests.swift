5bf424c2727558409090356da90f9b709499a05b
Nadya Post <nadya@bournedigital.com.au>
2020-04-16 17:20:33 +1000

    Autoregister tests

--- Bitcoin AdventurerTests/AutoregisterTests.swift
+++ Bitcoin AdventurerTests/AutoregisterTests.swift
@@ -54,6 +54,13 @@ class AutoregisterTests: XCTestCase {
   
   override func setUp() {
     super.setUp()
+    container.autoregister(Price.self,
+                           argument: String.self,
+                           initializer: Price.init(amount:))
+
+    container.autoregister(PriceResponse.self,
+                           argument: Price.self,
+                           initializer: PriceResponse.init(data:))
   }
   
   override func tearDown() {
@@ -64,11 +71,14 @@ class AutoregisterTests: XCTestCase {
   // MARK: - Tests
   
   func testPriceResponseData() {
-    XCTFail("Test not yet written.")
+    let price = container ~> (Price.self, argument: "789654")
+    let response = container ~> (PriceResponse.self, argument: price)
+    XCTAssertEqual(response.data.amount, "789654")
   }
   
   func testPrice() {
-    XCTFail("Test not yet written.")
+    let price = container ~> (Price.self, argument: "999456")
+    XCTAssertEqual(price.amount, "999456")
   }
 
 }
