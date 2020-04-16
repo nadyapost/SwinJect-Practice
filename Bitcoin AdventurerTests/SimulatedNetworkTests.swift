e75771ac497c9d7353714d6b94dc803f2904a624
Nadya Post <nadya@bournedigital.com.au>
2020-04-16 17:26:53 +1000

    Simulated networking test

--- Bitcoin AdventurerTests/SimulatedNetworkTests.swift
+++ Bitcoin AdventurerTests/SimulatedNetworkTests.swift
@@ -33,6 +33,27 @@ import Foundation
 
 @testable import Bitcoin_Adventurer
 
+struct SimulatedNetworking: Networking {
+  let filename: String
+
+  func request(from: Endpoint, completion: @escaping CompletionHandler) {
+    let data = readJSON(name: filename)
+    completion(data, nil)
+  }
+
+  private func readJSON(name: String) -> Data? {
+    let bundle = Bundle(for: SimulatedNetworkTests.self)
+    guard let url = bundle.url(forResource: name, withExtension: "json") else { return nil }
+
+    do {
+      return try Data(contentsOf: url, options: .mappedIfSafe)
+    }
+    catch {
+      XCTFail("Error occurred parsing test data")
+      return nil
+    }
+  }
+}
 enum DataSet: String {
     case one
     case two
@@ -53,6 +74,16 @@ class SimulatedNetworkTests: XCTestCase {
   
   override func setUp() {
     super.setUp()
+    container.autoregister(Networking.self,
+                           argument: String.self,
+                           initializer: SimulatedNetworking.init)
+
+    DataSet.all.forEach { dataSet in
+      container.register(BitcoinPriceFetcher.self, name: dataSet.name) { resolver in
+        let networking = resolver ~> (Networking.self, argument: dataSet.filename)
+        return BitcoinPriceFetcher(networking: networking)
+      }
+    }
   }
   
   override func tearDown() {
@@ -63,10 +94,26 @@ class SimulatedNetworkTests: XCTestCase {
   // MARK: - Tests
   
   func testDatasetOne() {
-    XCTFail("Test not yet written.")
+    let fetcher = container ~> (BitcoinPriceFetcher.self, name: DataSet.one.name)
+    let expectation = XCTestExpectation(description: "Fetch Bitcoin price from dataset one")
+        
+    fetcher.fetch { response in
+      XCTAssertEqual("100000.01", response!.data.amount)
+      expectation.fulfill()
+    }
+
+    wait(for: [expectation], timeout: 1.0)
   }
   
   func testDatasetTwo() {
-    XCTFail("Test not yet written.")
+    let fetcher = container ~> (BitcoinPriceFetcher.self, name: DataSet.two.name)
+    let expectation = XCTestExpectation(description: "Fetch Bitcoin price from dataset two")
+
+    fetcher.fetch { response in
+      XCTAssertEqual("9999999.76", response!.data.amount)
+      expectation.fulfill()
+    }
+
+    wait(for: [expectation], timeout: 1.0)
   }
 }
